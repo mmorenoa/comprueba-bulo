d3a0c11c610482761df5c5327fceb2eb37c45ecc
mmorenoa <m.morenoa@alumnos.upm.es>
2023-01-07 13:19:36 +0100

    Implementadas las diferentes vistas de la GUI, dependiendo de la fiabilidad #2

--- components/styled.js
+++ components/styled.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import { Colors } from './colors'
 
 export const Text = styled.span`
@@ -37,3 +37,26 @@ export const TopBar = styled.div`
 TopBar.defaultProps = {
     color: Colors.Blue
 }
+
+const rotate360 = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const Spinner = styled.div`
+  animation: ${rotate360} 1s linear infinite;
+  transform: translateZ(0);
+  
+  border-top: 2px solid grey;
+  border-right: 2px solid grey;
+  border-bottom: 2px solid grey;
+  border-left: 4px solid black;
+  background: transparent;
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+`;
--- popup.tsx
+++ popup.tsx
@@ -1,14 +1,58 @@
+import { useEffect, useState } from 'react';
 import { Colors } from '~components/colors';
-import { Container, Text, TopBar } from './components/styled';
+import { Container, Spinner, Text, TopBar } from './components/styled';
 
 function IndexPopup() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [reliability, setReliability] = useState("");
+  const [color, setColor] = useState(Colors.Blue);
+
+  useEffect(() => {
+    /*fetch('http://g1.etsisi.upm.es:8835/fact_checking/entailment?' + new URLSearchParams({
+    text: 'Las vacunas llevan grafeno'
+    }))*/
+    fetch('http://www.randomnumberapi.com/api/v1.0/random?min=0&max=10&count=10')
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      let sum = json.reduce((previous, current) => current += previous);
+      let avg = sum / json.length;
+      console.log(avg);
+      if(avg < 1) {
+        setReliability('Muy alta');
+        setColor(Colors.Green);
+      } else if(avg >= 1 && avg < 4) {
+        setReliability('Media - alta');
+        setColor(Colors.Yellow);
+      } else if(avg >= 4 && avg < 7.5) {
+        setReliability('Media');
+        setColor(Colors.Orange)
+      } else {
+        setReliability('Baja');
+        setColor(Colors.Red);
+      }
+      setIsLoading(false);
+    })
+    .catch(() => console.log("No se ha obtenido nada o algo ha fallado"));
+  },  []);
+
+  if(isLoading) {
+    return(
+      <Container>
+        <Spinner/>
+      </Container>
+    );
+  }
+
   return (
     <Container>
-      <TopBar color={Colors.Red}/>
+      <TopBar color={color}/>
       <Text>¡Información verídica!</Text>
       <div>
         <Text weight="400">Fiabilidad: </Text>
-        <Text color={Colors.Red}>Test</Text>
+        <Text color={color}>
+          {reliability}
+        </Text>
       </div>
     </Container>
   )
