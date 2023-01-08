fb9d3158b581f9d2631980fa2cfdd14375860de4
mmorenoa <m.morenoa@alumnos.upm.es>
2023-01-08 20:28:44 +0100

    Añadidas constantes para umbrales

--- src/popup.tsx
+++ src/popup.tsx
@@ -6,20 +6,24 @@ import useCheckingData from "~src/components/useCheckingData"
 import { Container, Spinner, Text, TopBar } from "./components/styled"
 
 function IndexPopup() {
+  const highThreshold = 1;
+  const midHighThreshold = 4;
+  const midThreshold = 7.5;
+
   const avg = useCheckingData("")
   const isLoading = avg === undefined
 
   const reliability = useMemo(() => {
-    if (avg < 1) return "Muy alta"
-    if (avg >= 1 && avg < 4) return "Media - alta"
-    if (avg >= 4 && avg < 7.5) return "Media"
+    if (avg < highThreshold) return "Muy alta"
+    if (avg >= highThreshold && avg < midHighThreshold) return "Media - alta"
+    if (avg >= midHighThreshold && avg < midThreshold) return "Media"
     return "Baja"
   }, [avg])
 
   const color = useMemo(() => {
-    if (avg < 1) return Colors.Green
-    if (avg >= 1 && avg < 4) return Colors.Yellow
-    if (avg >= 4 && avg < 7.5) return Colors.Orange
+    if (avg < highThreshold) return Colors.Green
+    if (avg >= highThreshold && avg < midHighThreshold) return Colors.Yellow
+    if (avg >= midHighThreshold && avg < midThreshold) return Colors.Orange
     return Colors.Red
   }, [avg])
 
