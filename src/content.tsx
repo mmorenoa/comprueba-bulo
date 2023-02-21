2d4e9c01d0c5daeeaac5adb0b0d7c3c1b008d7ae
mmorenoa <m.morenoa@alumnos.upm.es>
2023-02-21 09:09:56 +0100

    Ahora al seleccionar texto en una página, aparece un botón para abrir la extensión

--- src/content.tsx
+++ src/content.tsx
@@ -2,22 +2,18 @@ import React from "react"
 import { createRoot } from "react-dom/client"
 
 import Button from "./components/Button"
-import IndexPopup from "./popup"
 
 window.addEventListener("mouseup", () => {
-  if (document.getElementById("button-container") && document.getElementById("button-popup").onmouseenter ) {
-    //Si se hace "mouseup" y ya está mostrando el botón, se elimina el botón
+  if (
+    window.getSelection().toString() !== "" &&
+    !document.getElementById("button-container")
+  ) {
+    //Si se hace "mouseup", hay algo seleccionado y además no se existe el botón, se crea el botón
+    show(Button)
+    console.log("Botón mostrado")
+  } else if (window.getSelection().toString() === "") {//Si se hace "mouseup", no hay nada algo seleccionado y además no se existe el botón, se crea el botón
     document.getElementById("button-container").remove()
     console.log("Botón eliminado")
-  } else {
-    if (
-      window.getSelection().toString() !== "" &&
-      !document.getElementById("button-container")
-    ) {
-      //Si se hace "mouseup", hay algo seleccionado y además no se muestra el botón, se crea el botón
-      show(Button)
-      console.log("Botón mostrado")
-    }
   }
 })
 
@@ -35,5 +31,5 @@ const show = (element) => {
   document.body.appendChild(div)
   const root = createRoot(div)
   const DOMelement = element
-  root.render(<DOMelement/>)
+  root.render(<DOMelement />)
 }
