298b30d4b9bdf7c82fd3a61c0d5e94fc8ded7d05
mmorenoa <m.morenoa@alumnos.upm.es>
2023-04-04 12:10:18 +0200

    Colores corregidos para mejorar el contraste

--- src/styles/colors.tsx
+++ src/styles/colors.tsx
@@ -1,8 +1,8 @@
 export const Colors = {
   Blue: "#0C4A6E",
-  Green: "#16A34A",
-  Yellow: "#EAB308",
-  Orange: "#F97316",
+  Green: "#0F893D",
+  Yellow: "#BB8E1D",
+  Orange: "#EC7223",
   Red: "#DC2626",
   SecondaryText: "RGBA(12, 74, 110, 0.7)",
   DarkMode: {
@@ -11,24 +11,24 @@ export const Colors = {
   },
   Deuteranomaly: {
     Primary: "#235080",
-    Green: "#75914F",
-    Yellow: "#ECB20A",
+    Green: "#AB8961",
+    Yellow: "#816100",
     Orange: "#D78708",
     Red: "#AC5319",
     SecondaryText: "RGBA(35, 80, 128, 0.7)"
   },
   Protanomaly: {
     Primary: "#2C4F7B",
-    Green: "#6B9545",
-    Yellow: "#DCB90C",
+    Green: "#9D8F52",
+    Yellow: "#736611",
     Orange: "#C98D1D",
     Red: "#A15732",
     SecondaryText: "RGBA(44, 79, 123, 0.7)"
   },
   Tritanomaly: {
     Primary: "#05586C",
-    Green: "#329D84",
-    Yellow: "#F0AB75",
+    Green: "#3E9DA9",
+    Yellow: "#CC8E93",
     Orange: "#FB6E51",
     Red: "#DC2625",
     SecondaryText: "RGBA(5, 88, 108, 0.7)"
